44150ceeab66d90d6baf0bf83ae12654a3d145c3
Magdalena Brodala <magdalena.brodala.programista@gmail.com>
2023-03-16 15:22:16 +0100

    Added Auxiliary Methods

--- AuxiliaryMethods.cpp
+++ AuxiliaryMethods.cpp
@@ -1,6 +1,70 @@
 #include "PersonalBudget.h"
 #include "AuxiliaryMethods.h"
 
+string AuxiliaryMethods::convertIntegerToString(int number)
+{
+    ostringstream ss;
+    ss << number;
+    string str = ss.str();
+    return str;
+}
+
+string AuxiliaryMethods::loadLine()
+{
+    cin.sync();
+    string entry = "";
+    getline(cin, entry);
+    return entry;
+}
+
+/*string AuxiliaryMethods::pobierzLiczbe(string tekst, int pozycjaZnaku)
+{
+    string liczba = "";
+    while(isdigit(tekst[pozycjaZnaku]) == true)
+    {
+        liczba += tekst[pozycjaZnaku];
+        pozycjaZnaku ++;
+    }
+    return liczba;
+}*/
+
+string AuxiliaryMethods::changeFirstLetterForUpperCaseAndOthersForLowerCase(string text)
+{
+    if (!text.empty())
+    {
+        transform(text.begin(), text.end(), text.begin(), ::tolower);
+        text[0] = toupper(text[0]);
+    }
+    return text;
+}
+
+int AuxiliaryMethods::convertStringtoInteger(string number)
+{
+    int numberInt;
+    istringstream iss(number);
+    iss >> numberInt;
+
+    return numberInt;
+}
+
+int AuxiliaryMethods::loadInteger()
+{
+    string entry = "";
+    int number = 0;
+
+    while (true)
+    {
+        cin.sync();
+        getline(cin, entry);
+
+        stringstream myStream(entry);
+        if (myStream >> number)
+            break;
+        cout << "To nie jest liczba. Wpisz ponownie. " << endl;
+    }
+    return number;
+}
+
 char AuxiliaryMethods::loadChar()
 {
     string entry = "";
@@ -20,3 +84,4 @@ char AuxiliaryMethods::loadChar()
     }
     return sign;
 }
+
--- AuxiliaryMethods.h
+++ AuxiliaryMethods.h
@@ -1,9 +1,23 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
 
 class AuxiliaryMethods
 {
 public:
+    static string convertIntegerToString(int number);
+    static string loadLine();
+    //static string pobierzLiczbe(string tekst, int pozycjaZnaku);
+    static string changeFirstLetterForUpperCaseAndOthersForLowerCase(string text);
+    static int convertStringtoInteger(string number);
+    static int loadInteger();
     static char loadChar();
 };
+
+#endif
